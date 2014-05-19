a2c2764d474be27e8cd1902d607c618c7a75e08a
kuzel <michal.kuzel@cbox.cz>
2014-05-19 22:42:26 +0200

    module params

--- vrp6.c
+++ vrp6.c
@@ -1,16 +1,23 @@
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void) {
-  printk(KERN_ALERT "Hello, world\n");
+static char* fpgafn = "vrp_fpga.bin";
+module_param(fpgafn, charp, S_IRUGO);
+
+static int vrp6_init(void) {
+  printk(KERN_ALERT "vrp6 init\n");
   return 0;
 }
 
-static void hello_exit(void) {
-  printk(KERN_ALERT "Goodbye, cruel world\n");
+static void vrp6_exit(void) {
+  printk(KERN_ALERT "vrp6 exit\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(vrp6_init);
+module_exit(vrp6_exit);
 
