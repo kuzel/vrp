5abd146806c79ae8f9064cb2ffed10af4820ff0b
kuzel <michal.kuzel@cbox.cz>
2014-05-24 21:55:45 +0200

    open/close/read

--- vrp6.c
+++ vrp6.c
@@ -1,8 +1,12 @@
+#include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/stat.h>
+#include <linux/kernel.h>
+
+#include <asm/uaccess.h>	/* copy_*_user */
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -13,16 +17,71 @@ module_param( fpgafn, charp, S_IRUGO);
 module_param( burst_major, int, S_IRUGO);
 module_param( burst_minor, int, S_IRUGO);
 
+static struct burst_dev {
+	struct semaphore sem;     /* mutual exclusion semaphore     */
+	struct cdev cdev;	  /* Char device structure		*/
+} burst_device;
+
+static char message[100];
+static int cmessage;
+
+int burst_open(struct inode *inode, struct file *filp)
+{
+	struct burst_dev *dev; /* device information */
+	dev = container_of(inode->i_cdev, struct burst_dev, cdev);
+	filp->private_data = dev; /* for other methods */
+	printk("burst_open\n");
+	return 0;          /* success */
+}
+
+int burst_release(struct inode *inode, struct file *filp)
+{
+	printk("burst_release\n");
+	return 0;
+}
+
+ssize_t burst_read(struct file *filp, char __user *buf, size_t count,
+                loff_t *f_pos)
+{
+	struct burst_dev *dev = filp->private_data;
+	ssize_t retval = 0;
+	int len = 0;
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+
+	cmessage = jiffies;
+	len = sprintf(message, "%d\n", cmessage);
+	if (copy_to_user(buf, message, len)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	retval = len;
+
+  out:
+	up(&dev->sem);
+	return retval;
+}
+
+struct file_operations burst_fops = {
+	.owner =    THIS_MODULE,
+//	.llseek =   burst_llseek,
+	.read =     burst_read,
+//	.write =    burst_write,
+//	.unlocked_ioctl = burst_ioctl,
+	.open =     burst_open,
+	.release =  burst_release,
+};
+
 static int vrp6_init(void) {
-	dev_t dev = 0;
+	dev_t devno = 0;
 	int result;
 	if (burst_major) {
-		dev = MKDEV(burst_major, burst_minor);
-		result = register_chrdev_region(dev, 1, "burst");
+		devno = MKDEV(burst_major, burst_minor);
+		result = register_chrdev_region(devno, 1, "burst");
 	} else {
-		result = alloc_chrdev_region(&dev, burst_minor, 1, "burst");
-		burst_major = MAJOR(dev);
-		burst_minor = MINOR(dev);
+		result = alloc_chrdev_region(&devno, burst_minor, 1, "burst");
+		burst_major = MAJOR(devno);
+		burst_minor = MINOR(devno);
 	}
 	if (result < 0) {
 		printk(KERN_WARNING "vrp: can't get major %d\n", burst_major);
@@ -30,11 +89,23 @@ static int vrp6_init(void) {
 	}
 
 	printk(KERN_ALERT "vrp6 init, major %d, minor %d\n", burst_major, burst_minor);
+
+
+	sema_init(&burst_device.sem, 1);
+	cdev_init(&burst_device.cdev, &burst_fops);
+	burst_device.cdev.owner = THIS_MODULE;
+	burst_device.cdev.ops = &burst_fops;
+	result = cdev_add(&burst_device.cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (result)
+		printk(KERN_NOTICE "Error %d adding burst", result);
+
 	return 0;
 }
 
 static void vrp6_exit(void) {
 	dev_t devno = MKDEV(burst_major, burst_minor);
+	cdev_del(&burst_device.cdev);
 	unregister_chrdev_region(devno, 1);
 	printk(KERN_ALERT "vrp6 exit\n");
 }
